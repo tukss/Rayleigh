4ef2717c9bfd9ff1cc4f90528d2773721f71e77a
=FEATHERSTONE Nick <feathern@lcd.colorado.edu>
2014-08-11 18:44:38 -0600

    Fix_dtdr_top and fix_dtdr_bottom had been erroneousl omitted from the boundaryconditions namelist

--- benchmark/BoundaryConditions.F90
+++ benchmark/BoundaryConditions.F90
@@ -11,6 +11,7 @@ Module BoundaryConditions
     Real*8  :: dTdr_Top     = 0.0d0
     Real*8  :: dTdr_Bottom  = 0.0d0
 
-	Namelist /Boundary_Conditions_Namelist/ Fix_Tvar_Top, Fix_Tvar_Bottom, T_Bottom, T_Top, dTdr_top, dTdr_bottom
+	Namelist /Boundary_Conditions_Namelist/ Fix_Tvar_Top, Fix_Tvar_Bottom, T_Bottom, T_Top, dTdr_top, dTdr_bottom, &
+		fix_dtdr_bottom, fix_dtdr_top
 
 End Module BoundaryConditions
