3c3d3f167be6a7501637cc1e57b6257c22334240
Nick <feathern@colorado.edu>
2017-05-04 13:36:25 -0600

    Rechecked diffusive forces.  Wrapped them into Diagnostics_Linear_Forces where placeholders already existed. Implementing mean and fluctuating diffusive forces now.

--- src/Diagnostics/Diagnostics_Linear_Forces.F90
+++ src/Diagnostics/Diagnostics_Linear_Forces.F90
@@ -4,7 +4,7 @@
 #define END_DO enddo; enddo; enddo
 #define PSI k,r,t
 #define PSI2 r,t
-
+#define DDBUFF d2buffer%p3a
 Module Diagnostics_Linear_Forces
     Use Diagnostics_Base
     Implicit None
@@ -129,87 +129,213 @@ Contains
     End Subroutine Compute_Coriolis_Force
 
 
-
     Subroutine Compute_Viscous_Force(buffer)
         Implicit None
         Real*8, Intent(InOut) :: buffer(1:,my_r%min:,my_theta%min:,1:)
         Integer :: r,k, t
+        Real*8 :: mypi, amp,del2u, estress
+        Real*8, Allocatable :: mu_visc(:), dmudr(:), ovstheta(:), ovs2theta(:)
 
-        ! Placeholder
-        ! The radial force term can be grabbed easily enough following the solve.
-        ! 
-        ! The other two components are a bit complicated to calculate due to the higher order
-        ! derivatives involved.  Will return to this later...
-        ! It's  possible that this should be special routine, allowed to transform
-        ! back to ell-space for the derivatives.
-        If (compute_quantity(viscous_Force_r)) Then
+        Allocate(ovstheta(1:N_theta), ovs2theta(1:N_theta)) ! 1/sin; 1/sin^2
+        ovstheta = 1.0d0/sintheta
+        ovs2theta = 1.0d0/sin2theta
+
+        !Compute the dynamic viscosity mu=rho*nu (nu is kinematic viscosity)
+        Allocate(mu_visc(1:N_R), dmudr(1:N_R))
+
+        mu_visc = ref%density*nu
+        dmudr = mu_visc*ref%density*(ref%dlnrho+dlnu)
+
+
+        !////////////////////////////////////////////////////////
+
+        ! r-direction; Full
+        If (compute_quantity(viscous_force_r)) Then
+
             DO_PSI
-                qty(PSI) = 0.0d0
+                ! first, compute all the terms multiplied by mu
+                ! Del^2 {u_r}
+                del2u = DDBUFF(PSI,dvrdrdr)+Two_Over_R(r)*buffer(PSI,dvrdr)
+                del2u = del2u+OneOverRSquared(r)*(DDBUFF(PSI,dvrdtdt)+cottheta(t)*buffer(PSI,dvrdt))
+                del2u = del2u+OneOverRSquared(r)*DDBUFF(PSI,dvrdpdp)*ovs2theta(t)
+
+                !Add geometric terms to make this { Del^2{u} }_r
+                del2u = del2u-2.0d0*OneOverRsquared(r)*( &
+                        buffer(PSI,vr) + & 
+                        buffer(PSI,dvtdt)+buffer(PSI,vtheta)*cottheta(t) + &
+                        ovstheta(t)*buffer(PSI,dvpdp) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*(buffer(PSI,dvrdr)*ref%dlnrho(r)+ &
+                        buffer(PSI,vr)*ref%d2lnrho(r) )
+
+
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = buffer(PSI,dvrdr)-One_Third*buffer(PSI,vr)*ref%dlnrho(r)
+
+                qty(PSI) = 2.0d0*dmudr(r)*estress + mu_visc(r)*del2u
+
+
             END_DO
+
             Call Add_Quantity(qty)
         Endif
 
-        If (compute_quantity(viscous_pForce_r)) Then
+        ! r-direction; fluctuating
+        If (compute_quantity(viscous_pforce_r)) Then
+
             DO_PSI
-                qty(PSI) = 0.0d0
+                ! first, compute all the terms multiplied by mu
+                ! Del^2 {u_r}
+                del2u = d2_fbuffer(PSI,dvrdrdr)+Two_Over_R(r)*fbuffer(PSI,dvrdr)
+                del2u = del2u+OneOverRSquared(r)*(d2_fbuffer(PSI,dvrdtdt)+cottheta(t)*fbuffer(PSI,dvrdt))
+                del2u = del2u+OneOverRSquared(r)*d2_fbuffer(PSI,dvrdpdp)*ovs2theta(t)
+
+                !Add geometric terms to make this { Del^2{u} }_r
+                del2u = del2u-2.0d0*OneOverRsquared(r)*( &
+                        fbuffer(PSI,vr) + & 
+                        fbuffer(PSI,dvtdt)+fbuffer(PSI,vtheta)*cottheta(t) + &
+                        ovstheta(t)*fbuffer(PSI,dvpdp) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*(fbuffer(PSI,dvrdr)*ref%dlnrho(r)+ &
+                        fbuffer(PSI,vr)*ref%d2lnrho(r) )
+
+
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = fbuffer(PSI,dvrdr)-One_Third*fbuffer(PSI,vr)*ref%dlnrho(r)
+
+                qty(PSI) = 2.0d0*dmudr(r)*estress + mu_visc(r)*del2u
+
+
             END_DO
+
             Call Add_Quantity(qty)
         Endif
 
-        If (compute_quantity(viscous_mForce_r)) Then
+        ! r-direction; mean
+        If (compute_quantity(viscous_mforce_r)) Then
+
             DO_PSI
-                qty(PSI) = 0.0d0
+                ! first, compute all the terms multiplied by mu
+                ! Del^2 {u_r}
+                del2u = d2_m0(PSI2,dvrdrdr)+Two_Over_R(r)*m0_values(PSI2,dvrdr)
+                del2u = del2u+OneOverRSquared(r)*(d2_m0(PSI2,dvrdtdt)+cottheta(t)*m0_values(PSI2,dvrdt))
+                del2u = del2u+OneOverRSquared(r)*d2_m0(PSI2,dvrdpdp)*ovs2theta(t)
+
+                !Add geometric terms to make this { Del^2{u} }_r
+                del2u = del2u-2.0d0*OneOverRsquared(r)*( &
+                        m0_values(PSI2,vr) + & 
+                        m0_values(PSI2,dvtdt)+m0_values(PSI2,vtheta)*cottheta(t) + &
+                        ovstheta(t)*m0_values(PSI2,dvpdp) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*(m0_values(PSI2,dvrdr)*ref%dlnrho(r)+ &
+                        m0_values(PSI2,vr)*ref%d2lnrho(r) )
+
+
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = m0_values(PSI2,dvrdr)-One_Third*m0_values(PSI2,vr)*ref%dlnrho(r)
+
+                qty(PSI) = 2.0d0*dmudr(r)*estress + mu_visc(r)*del2u
+
+
             END_DO
+
+            Call Add_Quantity(qty)
+        Endif
+
+
+        !Theta-direction; Full
+        If (compute_quantity(viscous_force_theta)) Then
+
+            DO_PSI
+                ! first, compute all the terms multiplied by mu
+                ! Del^2 {u_theta}
+                del2u = DDBUFF(PSI,dvtdrdr)+Two_Over_R(r)*buffer(PSI,dvtdr)
+                del2u = del2u+OneOverRSquared(r)*(DDBUFF(PSI,dvtdtdt)+cottheta(t)*buffer(PSI,dvtdt))
+                del2u = del2u+OneOverRSquared(r)*DDBUFF(PSI,dvtdpdp)*ovs2theta(t)
+
+                !Add geometric terms to make this { Del^2{u} }_theta
+                del2u = del2u +OneOverRSquared(r)*( 2.0d0*buffer(PSI,dvrdt) - &
+                        ovs2theta(t)*(   buffer(PSI,vtheta) + &
+                        2.0d0*costheta(t)*buffer(PSI,dvpdp) ) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*One_Over_R(r)*buffer(PSI,dvrdt)*ref%dlnrho(r)
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = One_Over_R(r)*(buffer(PSI,dvrdt)-buffer(PSI,vtheta) ) +buffer(PSI,dvtdr)
+
+                qty(PSI) = dmudr(r)*estress +mu_visc(r)*del2u
+            END_DO
+
+            Call Add_Quantity(qty)
+        Endif
+
+        !Theta-direction; Fluctuating
+        If (compute_quantity(viscous_force_theta)) Then
+
+            DO_PSI
+                ! first, compute all the terms multiplied by mu
+                ! Del^2 {u_theta}
+                del2u = d2_fbuffer(PSI,dvtdrdr)+Two_Over_R(r)*fbuffer(PSI,dvtdr)
+                del2u = del2u+OneOverRSquared(r)*(d2_fbuffer(PSI,dvtdtdt)+cottheta(t)*fbuffer(PSI,dvtdt))
+                del2u = del2u+OneOverRSquared(r)*d2_fbuffer(PSI,dvtdpdp)*ovs2theta(t)
+
+                !Add geometric terms to make this { Del^2{u} }_theta
+                del2u = del2u +OneOverRSquared(r)*( 2.0d0*fbuffer(PSI,dvrdt) - &
+                        ovs2theta(t)*(   fbuffer(PSI,vtheta) + &
+                        2.0d0*costheta(t)*fbuffer(PSI,dvpdp) ) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*One_Over_R(r)*fbuffer(PSI,dvrdt)*ref%dlnrho(r)
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = One_Over_R(r)*(fbuffer(PSI,dvrdt)-fbuffer(PSI,vtheta) ) +fbuffer(PSI,dvtdr)
+
+                qty(PSI) = dmudr(r)*estress +mu_visc(r)*del2u
+            END_DO
+
             Call Add_Quantity(qty)
         Endif
 
 
 
-        If (compute_quantity(viscous_Force_theta)) Then
+        !Phi-direction
+        If (compute_quantity(viscous_force_phi)) Then
+
             DO_PSI
-                qty(PSI) = 0.0d0
+                del2u = DDBUFF(PSI,dvpdrdr)+Two_Over_R(r)*buffer(PSI,dvpdr)
+                del2u = del2u+OneOverRSquared(r)*(DDBUFF(PSI,dvpdtdt)+cottheta(t)*buffer(PSI,dvpdt))
+                del2u = del2u+OneOverRSquared(r)*DDBUFF(PSI,dvpdpdp)*ovs2theta(t)
+
+                ! Add geometric terms here
+                ! del2u - 
+                !Add geometric terms to make this { Del^2{u} }_phi
+                del2u = del2u +OneOverRSquared(r)*( 2.0d0*buffer(PSI,dvrdp)*ovstheta(t) - &
+                        ovs2theta(t)*(   buffer(PSI,vphi) - &
+                        2.0d0*costheta(t)*buffer(PSI,dvtdp) ) )
+
+                ! Move onto the non del squared terms (compressibility term)
+                del2u = del2u-One_Third*One_Over_R(r)*ovstheta(t)*buffer(PSI,dvrdp)*ref%dlnrho(r)
+
+                ! Finally, add the piece due to the gradient of mu
+                estress = One_Over_R(r)*(ovstheta(t)*buffer(PSI,dvrdp)-buffer(PSI,vphi) )+ &
+                          buffer(PSI,dvpdr)
+
+                qty(PSI) =dmudr(r)*estress + mu_visc(r)*del2u
             END_DO
+
             Call Add_Quantity(qty)
         Endif
 
-        If (compute_quantity(viscous_pForce_theta)) Then
-            DO_PSI
-                qty(PSI) = 0.0d0
-            END_DO
-            Call Add_Quantity(qty)
-        Endif
 
-        If (compute_quantity(viscous_mForce_theta)) Then
-            DO_PSI
-                qty(PSI) = 0.0d0
-            END_DO
-            Call Add_Quantity(qty)
-        Endif
-
-
-
-        If (compute_quantity(viscous_Force_phi)) Then
-            DO_PSI
-                qty(PSI) = 0.0d0
-            END_DO
-            Call Add_Quantity(qty)
-        Endif
-
-        If (compute_quantity(viscous_pForce_phi)) Then
-            DO_PSI
-                qty(PSI) = 0.0d0
-            END_DO
-            Call Add_Quantity(qty)
-        Endif
-
-        If (compute_quantity(viscous_mForce_phi)) Then
-            DO_PSI
-                qty(PSI) = 0.0d0
-            END_DO
-            Call Add_Quantity(qty)
-
-        Endif
-
+        DeAllocate(mu_visc, dmudr)
+        DeAllocate(ovstheta,ovs2theta)
     End Subroutine Compute_Viscous_Force
 
 End Module Diagnostics_Linear_Forces
